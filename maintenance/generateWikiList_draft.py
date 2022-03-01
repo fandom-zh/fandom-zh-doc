28e98db00ea6d0a3cb47f6be04e04bf8c49c34c2
Winston Sung <52590365+WinstonSung@users.noreply.github.com>
2022-03-01 18:44:58 +0800

    Update generateWikiList_draft.py

--- maintenance/generateWikiList_draft.py
+++ maintenance/generateWikiList_draft.py
@@ -8,9 +8,11 @@ languages = {
   "zh-tw": "zh-tw"
 }
 
-for key, value in languages:
+pattern = re.compile(r'<li>\n	<a href="http:\/\/([^"]*)\/">([^<]*)<\/a>\n			\(([^,]*),  ([^	]*)		Founder name: <a href="https:\/\/community\.fandom\.com\/wiki\/Special:CheckUser\?user=[^"]*">([^<]*)<\/a>\n		, Email: <a href="https:\/\/community\.fandom\.com\/wiki\/Special:LookupUser\?target=[^"]*">[^<]*<\/a>\n		, Confirm: [^	](	)*(, IP: <a href="https:\/\/community\.fandom\.com\/wiki\/Special:MultiLookup\?target=[^"]*">[^<]*<\/a>\)\n			)*<\/li>')
+
+for key, value in languages.items():
   r = requests.get("http://community.fandom.com/wiki/Special:Newwikis?start=&language=" + key + "&limit=5000")
   soup = BeautifulSoup(r.content, "html.parser")
-  rawHTML = soup.select_one('#mw-content-text .mw-spcontent ul')
-  csvData = re.sub(r'<li>\n	<a href="http:\/\/([^"]*)\/">([^<]*)<\/a>\n			\(([^,]*),  ([^	]*)		Founder name: <a href="https:\/\/community\.fandom\.com\/wiki\/Special:CheckUser\?user=[^"]*">([^<]*)<\/a>\n		, Email: <a href="https:\/\/community\.fandom\.com\/wiki\/Special:LookupUser\?target=[^"]*">[^<]*<\/a>\n		, Confirm: [^	](	)*(, IP: <a href="https:\/\/community\.fandom\.com\/wiki\/Special:MultiLookup\?target=[^"]*">[^<]*<\/a>\)\n			)*<\/li>', '"","https://$1","$2","$3","$4","$5","","",""', rawHTML)
+  rawHTML = str( soup.select_one('#mw-content-text .mw-spcontent ul') )
+  csvData = re.sub(pattern, '"","https://$1","$2","$3","$4","$5","","",""', rawHTML)
   print(csvData)
